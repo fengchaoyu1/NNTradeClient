13dabca9a6f0a6b32d922d7223a2faee42159470
cyberzhg <cyberzhg@gmail.com>
2015-03-08 17:12:25 +0800

    有ToolBar的空项目。
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

---
+++ app/src/main/java/zhaohg/trade/MainActivity.java
@@ -0,0 +1,27 @@
+package zhaohg.trade;
+
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+
+public class MainActivity extends ActionBarActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+}

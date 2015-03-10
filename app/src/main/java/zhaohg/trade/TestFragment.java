4a05b519ebc42078921324978afc655dfbd4f68d
cyberzhg <cyberzhg@gmail.com>
2015-03-10 19:27:25 +0800

    Add name for Sliding Tab.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- app/src/main/java/zhaohg/trade/TestFragment.java
+++ app/src/main/java/zhaohg/trade/TestFragment.java
@@ -1,24 +1,17 @@
 package zhaohg.trade;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import zhaohg.sliding.SlidingFragment;
 
-public class TestFragment extends Fragment {
-
-    public TestFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class TestFragment extends SlidingFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_test, container, false);
     }
+
 }

065199984bd8266fdd8939973eee00f0142c8660
dip_server_1 <dip_server_1@diplab.com>
2015-03-20 10:03:08 +0800

    Test login activity #19
    Signed-off-by: dip_server_1 <dip_server_1@diplab.com>

--- app/src/main/java/zhaohg/account/LoginActivity.java
+++ app/src/main/java/zhaohg/account/LoginActivity.java
@@ -1,15 +1,84 @@
 package zhaohg.account;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import zhaohg.api.ApiErrno;
+import zhaohg.api.account.AccountLogin;
+import zhaohg.api.account.AccountLoginPostEvent;
 import zhaohg.trade.R;
 import zhaohg.trade.TestableActivity;
 
 public class LoginActivity extends TestableActivity {
 
+    private ProgressBar progressBar;
+    private EditText editUsername;
+    private EditText editPassword;
+    private Button loginButton;
+    private TextView textError;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.progressBar = (ProgressBar) this.findViewById(R.id.login_progress);
+        this.editUsername = (EditText) this.findViewById(R.id.username);
+        this.editPassword = (EditText) this.findViewById(R.id.password);
+        this.loginButton = (Button) this.findViewById(R.id.login_button);
+        this.textError = (TextView) this.findViewById(R.id.text_error);
+
+        this.loginButton.setOnClickListener(new OnLoginClick());
     }
+
+    private void hideErrorMessage() {
+        this.textError.setVisibility(View.GONE);
+        this.textError.setText("");
+    }
+
+    private void showErrorMessage(String text) {
+        this.textError.setVisibility(View.VISIBLE);
+        this.textError.setText(text);
+    }
+
+    private class OnLoginClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            final Context context = getApplicationContext();
+            hideErrorMessage();
+            final String username = editUsername.getText().toString();
+            final String password = editPassword.getText().toString();
+            if (username.isEmpty() || password.isEmpty()) {
+                showErrorMessage(context.getString(R.string.error_field_required));
+                finishTest();
+                return;
+            }
+            AccountLogin login = new AccountLogin(context);
+            login.setParameter(username, password);
+            login.setEvent(new AccountLoginPostEvent() {
+                @Override
+                public void onSuccess() {
+                    progressBar.setVisibility(View.GONE);
+                    finishTest();
+                }
+
+                @Override
+                public void onFailure(int errno) {
+                    progressBar.setVisibility(View.GONE);
+                    loginButton.setEnabled(true);
+                    showErrorMessage(ApiErrno.getErrorMessage(context, errno));
+                    finishTest();
+                }
+            });
+            progressBar.setVisibility(View.VISIBLE);
+            loginButton.setEnabled(false);
+            login.request();
+        }
+    }
+
 }
---
+++ app/src/main/java/zhaohg/test/account/TestLoginActivity.java
@@ -0,0 +1,239 @@
+package zhaohg.test.account;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.concurrent.CountDownLatch;
+
+import zhaohg.account.LoginActivity;
+import zhaohg.api.Encryption;
+import zhaohg.api.account.AccountRegister;
+import zhaohg.api.account.AccountRegisterPostEvent;
+import zhaohg.trade.OnTestFinishedListener;
+import zhaohg.trade.R;
+
+public class TestLoginActivity extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    private LoginActivity activity;
+
+    private EditText editUsername;
+    private EditText editPassword;
+    private Button loginButton;
+    private TextView textError;
+
+    public TestLoginActivity() {
+        super(LoginActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        this.activity = this.getActivity();
+
+        this.editUsername = (EditText) this.activity.findViewById(R.id.username);
+        this.editPassword = (EditText) this.activity.findViewById(R.id.password);
+        this.loginButton = (Button) this.activity.findViewById(R.id.login_button);
+        this.textError = (TextView) this.activity.findViewById(R.id.text_error);
+    }
+
+    private String generateRandomName() {
+        Calendar calendar = Calendar.getInstance();
+        String text = "login_activity_" + calendar.getTimeInMillis();
+        return "login_activity_" + Encryption.md5(text);
+    }
+
+    public void testLoginNormal() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        AccountRegister register = new AccountRegister(this.activity.getApplicationContext());
+        final String existedUsername = this.generateRandomName();
+        final String existedPassword = "existed";
+        register.setParameter(existedUsername, existedPassword);
+        register.setEvent(new AccountRegisterPostEvent() {
+            @Override
+            public void onSuccess() {
+                activity.runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                editUsername.setText(existedUsername);
+                                editPassword.setText(existedPassword);
+                                activity.setOnTestFinishedListener(new OnTestFinishedListener() {
+                                    @Override
+                                    public void onTaskFinished() {
+                                        signal.countDown();
+                                    }
+                                });
+                                loginButton.performClick();
+                            }
+                        }
+                );
+            }
+            @Override
+            public void onFailure(int errno) {
+                signal.countDown();
+            }
+        });
+        register.request();
+        signal.await();
+        assertFalse(this.loginButton.isEnabled());
+        assertEquals(View.GONE, this.textError.getVisibility());
+    }
+
+    public void testLoginEmptyName() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        AccountRegister register = new AccountRegister(this.activity.getApplicationContext());
+        final String existedUsername = this.generateRandomName();
+        final String existedPassword = "existed";
+        register.setParameter(existedUsername, existedPassword);
+        register.setEvent(new AccountRegisterPostEvent() {
+            @Override
+            public void onSuccess() {
+                activity.runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                editUsername.setText("");
+                                editPassword.setText(existedPassword);
+                                activity.setOnTestFinishedListener(new OnTestFinishedListener() {
+                                    @Override
+                                    public void onTaskFinished() {
+                                        signal.countDown();
+                                    }
+                                });
+                                loginButton.performClick();
+                            }
+                        }
+                );
+            }
+            @Override
+            public void onFailure(int errno) {
+                signal.countDown();
+            }
+        });
+        register.request();
+        signal.await();
+        assertTrue(this.loginButton.isEnabled());
+        assertEquals(View.VISIBLE, this.textError.getVisibility());
+        assertEquals(this.activity.getString(R.string.error_field_required), this.textError.getText());
+    }
+
+    public void testLoginEmptyPassword() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        AccountRegister register = new AccountRegister(this.activity.getApplicationContext());
+        final String existedUsername = this.generateRandomName();
+        final String existedPassword = "existed";
+        register.setParameter(existedUsername, existedPassword);
+        register.setEvent(new AccountRegisterPostEvent() {
+            @Override
+            public void onSuccess() {
+                activity.runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                editUsername.setText(existedUsername);
+                                editPassword.setText("");
+                                activity.setOnTestFinishedListener(new OnTestFinishedListener() {
+                                    @Override
+                                    public void onTaskFinished() {
+                                        signal.countDown();
+                                    }
+                                });
+                                loginButton.performClick();
+                            }
+                        }
+                );
+            }
+            @Override
+            public void onFailure(int errno) {
+                signal.countDown();
+            }
+        });
+        register.request();
+        signal.await();
+        assertTrue(this.loginButton.isEnabled());
+        assertEquals(View.VISIBLE, this.textError.getVisibility());
+        assertEquals(this.activity.getString(R.string.error_field_required), this.textError.getText());
+    }
+
+    public void testLoginNonExistName() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        AccountRegister register = new AccountRegister(this.activity.getApplicationContext());
+        final String existedUsername = this.generateRandomName();
+        final String existedPassword = "existed";
+        register.setParameter(existedUsername, existedPassword);
+        register.setEvent(new AccountRegisterPostEvent() {
+            @Override
+            public void onSuccess() {
+                activity.runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                editUsername.setText(existedUsername + "_non");
+                                editPassword.setText(existedPassword);
+                                activity.setOnTestFinishedListener(new OnTestFinishedListener() {
+                                    @Override
+                                    public void onTaskFinished() {
+                                        signal.countDown();
+                                    }
+                                });
+                                loginButton.performClick();
+                            }
+                        }
+                );
+            }
+            @Override
+            public void onFailure(int errno) {
+                signal.countDown();
+            }
+        });
+        register.request();
+        signal.await();
+        assertTrue(this.loginButton.isEnabled());
+        assertEquals(View.VISIBLE, this.textError.getVisibility());
+        assertEquals(this.activity.getString(R.string.errno_username_non_exist), this.textError.getText());
+    }
+
+    public void testLoginWrongPassword() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        AccountRegister register = new AccountRegister(this.activity.getApplicationContext());
+        final String existedUsername = this.generateRandomName();
+        final String existedPassword = "existed";
+        register.setParameter(existedUsername, existedPassword);
+        register.setEvent(new AccountRegisterPostEvent() {
+            @Override
+            public void onSuccess() {
+                activity.runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                editUsername.setText(existedUsername);
+                                editPassword.setText(existedPassword + "_non");
+                                activity.setOnTestFinishedListener(new OnTestFinishedListener() {
+                                    @Override
+                                    public void onTaskFinished() {
+                                        signal.countDown();
+                                    }
+                                });
+                                loginButton.performClick();
+                            }
+                        }
+                );
+            }
+            @Override
+            public void onFailure(int errno) {
+                signal.countDown();
+            }
+        });
+        register.request();
+        signal.await();
+        assertTrue(this.loginButton.isEnabled());
+        assertEquals(View.VISIBLE, this.textError.getVisibility());
+        assertEquals(this.activity.getString(R.string.errno_mismatch_username_password), this.textError.getText());
+    }
+
+}

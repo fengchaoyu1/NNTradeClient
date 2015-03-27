c9a714bc922b1c6f653490cd30c6feb225dce00c
chaoyu <364402956@qq.com>
2015-03-26 17:09:01 -0700

    add downloadtest for picture
    Signed-off-by: chaoyu <364402956@qq.com>

---
+++ app/src/main/java/com/example/dip/testapplication/test/DownloadTest.java
@@ -0,0 +1,68 @@
+package com.example.dip.testapplication.test;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.test.InstrumentationTestCase;
+import android.widget.ImageView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Dip on 2015/3/26.
+ */
+public class DownloadTest extends InstrumentationTestCase{
+    ImageView myView3=null;
+    private class RequestTask extends AsyncTask<String, Integer, Bitmap> {
+
+
+        protected Bitmap doInBackground(String ...imagePath) {
+
+            HttpGet httpRequest =new HttpGet(imagePath[0]);
+            HttpClient httpClient=new DefaultHttpClient();
+            Bitmap bitmap=null;
+            try{
+                HttpResponse httpResponse=httpClient.execute(httpRequest);
+                if(httpResponse.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+                    HttpEntity httpEntity=httpResponse.getEntity();
+                    InputStream is = httpEntity.getContent();
+                    bitmap = BitmapFactory.decodeStream(is);
+                    is.close();
+
+
+
+                }
+
+            }
+            catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            return bitmap;
+
+        }
+        protected void onPostExecute(Bitmap bitmap) {
+
+            myView3.setImageBitmap(bitmap);
+        }
+
+    }
+    public void testDownload() throws Exception {
+        String imagePath="http://ts2.mm.bing.net/th?id=HN.608007102913186522&pid=1.7";
+        RequestTask requestTask=new RequestTask();
+        requestTask.execute(imagePath);
+        assertFalse(imagePath.equals(null));
+        assertFalse(requestTask.equals(null));
+    }
+}
--- app/src/main/java/com/example/dip/testapplication/test/ThumbnailsTest.java
+++ app/src/main/java/com/example/dip/testapplication/test/ThumbnailsTest.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import com.example.dip.testapplication.Creatpicture;
+//import com.example.dip.testapplication.Creatpicture;
 
 
 import static junit.framework.Assert.assertFalse;
@@ -48,10 +48,52 @@ public class ThumbnailsTest extends InstrumentationTestCase{
         bmap= ThumbnailUtils.extractThumbnail(bmap, width, height, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
         return bmap;
     }
+    public int[] initColors(int STRIDE, int HEIGHT, int WIDTH) {
+        int[] colors=new int[STRIDE*HEIGHT];
+        for (int y = 0; y < HEIGHT; y++) {//use of x,y is legible then the use of i,j
+            for (int x = 0; x < WIDTH; x++) {
+                int r = x * 255 / (WIDTH - 1);
+                int g = y * 255 / (HEIGHT - 1);
+                int b = 255 - Math.min(r, g);
+                int a = Math.max(r, g);
+                colors[y*STRIDE + x]=(a<<24)|(r<<16)|(g<<8)|(b);//the shift operation generates the color ARGB
+            }
+        }
+        return colors;
+    }
+    public void Creatpicture(){
+        Bitmap mBitmapBig = null;
+        int[] mColors;
+        int WIDTH=300;
+        int HEIGHT=400;
+        int STRIDE=500;//must be >=WIDTH
+        String IMAGE_NAME="abc";
+
+
+        mColors=initColors(STRIDE,HEIGHT,WIDTH);
+        mBitmapBig=Bitmap.createBitmap(mColors, 0, STRIDE, WIDTH, HEIGHT, Bitmap.Config.RGB_565);
+        File file1 = new File(Environment.getExternalStorageDirectory() + "/"+IMAGE_NAME + ".jpg");
+        FileOutputStream out=null;
+        try {
+            out = new FileOutputStream(file1);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+
+            mBitmapBig.compress(Bitmap.CompressFormat.JPEG, 30, out);
+            out.flush();
+            out.close();
+
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
     public void testThumbnailsBig() throws Exception {
-        Creatpicture creat= new Creatpicture();
+        Creatpicture();
 
         Bitmap mBitmap=null;
         String ImagePath=Environment.getExternalStorageDirectory() + "/"+"abc" + ".jpg";
@@ -74,30 +116,7 @@ public class ThumbnailsTest extends InstrumentationTestCase{
     }
 
     public void testThumbnailsSmall() throws Exception{
-        /*int[] mColors;
-        int WIDTH=75;
-        int HEIGHT=100;
-        int STRIDE=150;//must be >=WIDTH
-        String IMAGENAME="abc";
-        Bitmap mBitmap=null;
-
-        mColors=initColors(STRIDE,HEIGHT,WIDTH);
-        mBitmapSmall=Bitmap.createBitmap(mColors, 0, STRIDE, WIDTH, HEIGHT, Bitmap.Config.RGB_565);
-        File file1 = new File(Environment.getExternalStorageDirectory() + "/"+IMAGENAME + ".jpg");
-        FileOutputStream out =new FileOutputStream(file1);
-        try {
-
-            mBitmapSmall.compress(Bitmap.CompressFormat.JPEG, 30, out);
-            out.flush();
-            out.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertFalse(out.equals(null));*/
-        Creatpicture creat= new Creatpicture();
+        Creatpicture();
         Bitmap mBitmap=null;
         String ImagePath=Environment.getExternalStorageDirectory() + "/"+"abc" + ".jpg";
         mBitmap=getImageThumbnail(ImagePath, 150, 200);

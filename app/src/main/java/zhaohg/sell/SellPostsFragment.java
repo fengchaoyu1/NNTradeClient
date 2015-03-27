9a4dcc63f22066f1ac1b07dfcc79889c75b84051
CyberZHG <CyberZHG@gmail.com>
2015-03-27 22:03:21 +0800

    Pull to load more.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- app/src/main/java/zhaohg/sell/SellPostsFragment.java
+++ app/src/main/java/zhaohg/sell/SellPostsFragment.java
@@ -19,13 +19,17 @@ import zhaohg.api.sell.SellPost;
 import zhaohg.main.R;
 import zhaohg.testable.TestableFragment;
 
+import static android.support.v7.widget.RecyclerView.OnScrollListener;
+
 public class SellPostsFragment extends TestableFragment {
 
     private Context context;
 
+    private LinearLayoutManager layoutManager;
     private RecyclerView recyclePosts;
     private ImageView imageNewPost;
 
+    private boolean loading = false;
     private int pageNum = 1;
 
     public SellPostsFragment() {
@@ -40,10 +44,14 @@ public class SellPostsFragment extends TestableFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sell_posts, container, false);
         this.context = view.getContext();
+
         this.recyclePosts = (RecyclerView) view.findViewById(R.id.recycle_posts);
-        this.recyclePosts.setLayoutManager(new LinearLayoutManager(context));
+        this.layoutManager = new LinearLayoutManager(context);
+        this.recyclePosts.setLayoutManager(layoutManager);
         SellPostsAdapter adapter = new SellPostsAdapter(context);
         this.recyclePosts.setAdapter(adapter);
+        this.recyclePosts.setOnScrollListener(new OnPostsScrollListener());
+
         this.imageNewPost = (ImageView) view.findViewById(R.id.image_new);
         this.imageNewPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +62,13 @@ public class SellPostsFragment extends TestableFragment {
                 context.startActivity(intent);
             }
         });
+
         loadNextPage();
         return view;
     }
 
     public void loadNextPage() {
+        loading = true;
         GetSellPostList getSellPostList = new GetSellPostList(context);
         getSellPostList.setParameter(pageNum);
         getSellPostList.setEvent(new GetSellPostListPostEvent() {
@@ -67,15 +77,39 @@ public class SellPostsFragment extends TestableFragment {
                 SellPostsAdapter adapter = (SellPostsAdapter) recyclePosts.getAdapter();
                 adapter.append(posts);
                 ++pageNum;
+                loading = false;
                 finishTest();
             }
             @Override
             public void onFailure(int errno) {
                 Toast.makeText(context, context.getString(R.string.sell_get_posts_fail), Toast.LENGTH_SHORT).show();
+                loading = false;
                 finishTest();
             }
         });
         getSellPostList.request();
+    }
+
+    private class OnPostsScrollListener extends OnScrollListener {
+
+        private int pastVisibleItems;
+        private int visibleItemCount;
+        private int totalItemCount;
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            visibleItemCount = layoutManager.getChildCount();
+            totalItemCount = layoutManager.getItemCount();
+            pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+            if (!loading) {
+                if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                    loadNextPage();
+                }
+            }
+        }
+
     }
 
 }
